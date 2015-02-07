1745f1ed3a4904c4f9c3147d1462dd17319a6318
Sasha Gerrand <github.com-grapevine@sgerrand.com>
2015-02-07 17:31:54 +0000

    Removed unused requires, updated RSpec config
    Removed references to RubyGems and Rack, as they're unrequired.
    Changed grapevine reference to rely on load path ordering - this should
    be ordered correctly during the test phase.
    
    Added some sane RSpec configuration defaults.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,19 @@
-require 'rubygems'
-
 if RUBY_VERSION == "1.9.3"
   require 'debugger'
 elsif RUBY_VERSION >= "2.0.0"
   require 'byebug'
 end
 
-require 'rack'
-
-require File.expand_path('../../lib/grapevine', __FILE__)
+require 'grapevine'
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.order = :random
 
   def destination_root
     File.join(File.dirname(__FILE__), 'sandbox')
